6cce4780594b2a5ed35c5cea0c43f51496d680f4
brunoschadeck <bruno.schadeck@deutschebahn.com>
2024-10-02 17:17:29 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# vue-template
-About A lean vue template for testing DB UI v3 components.
+# DB UX Design System Vue Template
+
+## Install
+
+First you have to install all node packages:
+
+`npm install`
+
+## Start
+
+In the project directory, you can run:
+
+`npm run serve`
+
+## Develop
+
+All packages are already installed and integrated so that you can directly import and use the components of the **DB UX Design System**.
+
+Simply use the `App.vue` as a starting point.
+
+
+## Have fun ❤️
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <DBSection width="large">
+    <h1>DB UI Playground</h1>
+    <p>Test the components of the DB UX Design System here.</p>
+    <DBLink
+      href="https://marketingportal.extranet.deutschebahn.com/marketingportal/Design-Anwendungen/db-ux-design-system"
+      content="external"
+    >
+      Docs
+    </DBLink>
+  </DBSection>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { DBSection, DBLink } from "@db-ui/v-components";
+
+export default defineComponent({
+  name: "App",
+  components: { DBSection, DBLink },
+});
+</script>
+
+<style>
+/* add styles */
+</style>
